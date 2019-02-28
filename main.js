cf9ef7cca3a4f4c865a3ab6d3189e889315e0fbb
Mohammad <mfg1376@gmail.com>
2019-02-28 12:30:14 +0330

    use mediaelement

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // You can use either a string for the player ID (i.e., `player`), 
 // or `document.querySelector()` for any selector
-var player = new MediaElementPlayer('player', {
+var player = new MediaElementPlayer('player1', {
     pluginPath: "./node_modules/mediaelement/build/",
-    features: ['quality'],
-    qualityText: "quality",
+    // features: ['quality', 'controls'],
+    // qualityText: "quality",
     // When using `MediaElementPlayer`, an `instance` argument
     // is available in the `success` callback
     success: function (mediaElement, originalNode, instance) {
